c383b94ebf23c31fc6ca7608804300352401dc11
NikolozChachanidze <chachanidzenika1997@gmail.com>
2026-02-06 12:21:18 +0400

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,66 @@
+﻿class Program
+{
+    static void Main()
+    {
+        //int[] myArray = { 92, 6, 73, -77, 81, -90, 99, 8, -85, 34 };
+
+        //int[] myArray2 = CountPosSumNeg(myArray);
+
+        //foreach (var item in myArray2)
+        //{
+        //    Console.WriteLine(item);
+        //}
+
+        //---------------------------------------------------------------------
+
+        //Console.WriteLine(GetMiddle("test"));
+        //Console.WriteLine(GetMiddle("chiqa"));
+
+        //-------------------------------------------------------------------
+
+        //Console.WriteLine(SortDescending(784));
+    }
+
+    //static int[] CountPosSumNeg(int[] arr)
+    //{
+    //    int positiveCount = 0;
+    //    int negativeSum = 0;
+
+    //    foreach (var item in arr)
+    //    {
+    //        if (item > 0)
+    //            positiveCount++;
+    //        else if (item < 0)
+    //            negativeSum += item;
+    //    }
+
+    //    return new int[] { positiveCount, negativeSum };
+    //}
+
+    //-----------------------------------------------------------------------------
+
+    //static string GetMiddle(string word)
+    //{
+    //    int length = word.Length;
+    //    int middle = length / 2;
+
+    //    if(length % 2 == 0)
+    //    {
+    //        return word.Substring(middle - 1, 2);
+    //    }
+    //    else
+    //    {
+    //        return word.Substring(middle, 1);
+    //    }
+    //}
+
+    //-------------------------------------------------------------------------------
+
+    //static int SortDescending(int number)
+    //{
+    //    return int.Parse(
+    //        string.Concat(
+    //            number.ToString(
+    //                ).OrderByDescending(x => x)));
+    //}
+}
